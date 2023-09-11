b8b99ca457b029b095d8995a5a4e6a07616c76d8
蒋艺 <meqt@foxmail.com>
2023-09-11 15:32:57 +0800

    Remove target

--- Package.swift
+++ Package.swift
@@ -19,14 +19,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "CSE_167",
-            dependencies: []),
-        .testTarget(
-            name: "CSE_167Tests",
-            dependencies: ["CSE_167"]),
         .executableTarget(
             name: "HW0",
             linkerSettings: [
